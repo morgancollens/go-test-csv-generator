61f46789f82754d9382ea71522a3374a6da354bd
Morgan Collens <morgan@criticalpanda.com>
2024-12-18 13:19:23 -0800

    :sparkles: Added cli flag capture logic to main function

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	rows := flag.Int("rows", 1, "Number of rows to include in the generated CSV file.")
+	fields := flag.String("fields", "name,age", "Comma separated list of fields (ex. 'name, age, email') to include in the generated CSV file.")
+	filename := flag.String("filename", "output.csv", "Name of the file to write the generated CSV data to.")
+
+	flag.Parse()
+
+	fmt.Printf("Rows: %d\n", *rows)
+	fmt.Printf("Fields: %s\n", *fields)
+	fmt.Printf("Filename: %s\n", *filename)
 }
