030cc1971ba9faa43f7fd9f8300b398ea95912ea
Morgan Collens <morgan@criticalpanda.com>
2024-12-23 08:03:23 -0800

    :recycle: Refactored code in main file to inject certain dependencies for better support during testing

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -13,6 +12,31 @@ import (
 
 	"github.com/brianvoe/gofakeit/v7"
 )
+
+type FileHandler interface {
+	MkDirAll(path string, perm os.FileMode) error
+	Create(name string) (*os.File, error)
+}
+
+type OSFileHandler struct{}
+
+func (c OSFileHandler) MkDirAll(path string, perm os.FileMode) error {
+	return os.MkdirAll(path, perm)
+}
+
+func (c OSFileHandler) Create(name string) (*os.File, error) {
+	return os.Create(name)
+}
+
+type FileWriter interface {
+	Write(record []string, writer *csv.Writer) error
+}
+
+type CSVFileWriter struct{}
+
+func (c CSVFileWriter) Write(record []string, writer *csv.Writer) error {
+	return writer.Write(record)
+}
 
 var validFields = map[string]bool{
 	"name":       true,
@@ -88,15 +112,18 @@ func generateBaseFields() BaseFields {
 	}
 }
 
-func generateCsvData(rows int, fields string, outputDir string, filename string) error {
-	fieldSlice := strings.Split(fields, ",")
+type DataGenerator interface {
+	generateCsvData(rows int, fields string, outputDir string, filename string, fileHandler FileHandler, csvWriter FileWriter) error
+}
+type CSVDataGenerator struct{}
 
-	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+func (d CSVDataGenerator) generateCsvData(rows int, fields string, outputDir string, filename string, fileHandler FileHandler, csvWriter FileWriter) error {
+	if err := fileHandler.MkDirAll(outputDir, os.ModePerm); err != nil {
 		return fmt.Errorf("failed to create directory: %v", err)
 	}
 
 	filePath := filepath.Join(outputDir, filename)
-	file, err := os.Create(filePath)
+	file, err := fileHandler.Create(filePath)
 	if err != nil {
 		return err
 	}
@@ -105,7 +132,9 @@ func generateCsvData(rows int, fields string, outputDir string, filename string)
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	if err := writer.Write(fieldSlice); err != nil {
+	fieldSlice := strings.Split(fields, ",")
+
+	if err := csvWriter.Write(fieldSlice, writer); err != nil {
 		return fmt.Errorf("failed to write header row: %v", err)
 	}
 
@@ -116,7 +145,7 @@ func generateCsvData(rows int, fields string, outputDir string, filename string)
 			row = append(row, generators[field](baseFields))
 		}
 
-		if err := writer.Write(row); err != nil {
+		if err := csvWriter.Write(row, writer); err != nil {
 			return fmt.Errorf("failed to write row: %v", err)
 		}
 	}
@@ -124,24 +153,16 @@ func generateCsvData(rows int, fields string, outputDir string, filename string)
 	return nil
 }
 
-func main() {
-	rows := flag.Int("rows", 1, "Number of rows to include in the generated CSV file.")
-	fields := flag.String("fields", "name,age", "Comma separated list of fields (ex. 'name,age,email') to include in the generated CSV file.")
-	filename := flag.String("filename", "output.csv", "Name of the file to write the generated CSV data to.")
-	seed := flag.Int("seed", 0, "Seed for random number generation.")
-
-	flag.Parse()
-
+func generate(
+	fileHandler FileHandler,
+	writer FileWriter,
+	generator DataGenerator,
+	rows *int,
+	fields *string,
+	filename *string,
+	seed *int,
+) {
 	startTime := time.Now()
-
-	if err := validateFlags(*rows, *fields, *filename); err != nil {
-		log.Fatalf("Invalid flags: %v", err)
-	}
-
-	invalidFields := validateSelectedFields(*fields)
-	if len(invalidFields) > 0 {
-		log.Fatalf("Unable to generate CSV data. Invalid fields selected: %s", strings.Join(invalidFields, ", "))
-	}
 
 	fmt.Printf("Rows: %d\n", *rows)
 	fmt.Printf("Fields: %s\n", *fields)
@@ -151,9 +172,35 @@ func main() {
 	gofakeit.Seed(*seed)
 
 	outputDir := "output"
-	generateCsvData(*rows, *fields, outputDir, *filename)
+	if err := generator.generateCsvData(*rows, *fields, outputDir, *filename, fileHandler, writer); err != nil {
+		panic(fmt.Sprintf("Failed to generate CSV data: %v", err))
+	}
 
 	elapsed := time.Since(startTime)
 
-	fmt.Printf("CSV file generated at %s/%s. (Elapsed time: %f seconds)\n", outputDir, *filename, elapsed.Seconds())
+	fmt.Printf("CSV file successfully generated at %s/%s.\n", outputDir, *filename)
+	fmt.Printf("(Elapsed time: %f seconds)\n", elapsed.Seconds())
+}
+
+func main() {
+	fileHandler := OSFileHandler{}
+	csvWriter := CSVFileWriter{}
+	generator := CSVDataGenerator{}
+
+	rows := flag.Int("rows", 1, "Number of rows to include in the generated CSV file.")
+	fields := flag.String("fields", "name,age", "Comma separated list of fields (ex. 'name,age,email') to include in the generated CSV file.")
+	filename := flag.String("filename", "output.csv", "Name of the file to write the generated CSV data to.")
+	seed := flag.Int("seed", 0, "Seed for random number generation.")
+	flag.Parse()
+
+	if err := validateFlags(*rows, *fields, *filename); err != nil {
+		panic(fmt.Sprintf("Invalid flags: %v", err))
+	}
+
+	invalidFields := validateSelectedFields(*fields)
+	if len(invalidFields) > 0 {
+		panic(fmt.Sprintf("Unable to generate CSV data. Invalid fields selected: %s", strings.Join(invalidFields, ", ")))
+	}
+
+	generate(fileHandler, csvWriter, generator, rows, fields, filename, seed)
 }
